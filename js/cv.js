d42b400f024bc190eed6f01a0055e9e797a234fb
Zakarya <zakarya.elkhattabi@gmail.com>
2020-04-07 00:49:03 +0200

    Problème scrollbar horizontal (cause => hr footer) & léger redesign

--- js/cv.js
+++ js/cv.js
@@ -2,7 +2,11 @@
 
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction()
+  navDisplay()
+
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -18,12 +22,15 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-// function changeHeadingBg(width){
-//   document.getElementById("entetediv1").style.width = width;
-// }
+ let styleOfNav=" display: -webkit-box;display: -ms-flexbox; display: flex; height: 70px; position: fixed;width: 100%; z-index: 100; background-color: rgba(0, 0, 0, 0.91);";
 
-// function erase(efface){
-//   document.getElementById("entetediv1").style.background= efface;
-// }
+//  function navDisplay(){
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("nav").style="display:none;";
+//   } 
+//   else {
+//     document.getElementById("nav").style=`${styleOfNav}`;
 
+//   }
+// }
 
